704f80d981ddcbc6044f3b015837e4570f066eb7
Jeremy <elbarto_860@hotmail.com>
2022-01-10 10:19:34 +0100

    add test for 30 minutes or less without paying and implement the feature in FareCalculatorService

--- src/main/java/com/parkit/parkingsystem/constants/Fare.java
+++ src/main/java/com/parkit/parkingsystem/constants/Fare.java
@@ -3,4 +3,6 @@ package com.parkit.parkingsystem.constants;
 public class Fare {
     public static final double BIKE_RATE_PER_HOUR = 1.0;
     public static final double CAR_RATE_PER_HOUR = 1.5;
+    public static final double BIKE_PRICE_30MIN_OR_LESS = 0.0;
+    public static final double CAR_PRICE_30MIN_OR_LESS = 0.0;
 }
--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,26 +5,35 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
+	public void calculateFare(Ticket ticket) {
+		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
+		}
 
-        long inMillis = ticket.getInTime().getTime();
-        long outMillis = ticket.getOutTime().getTime();
+		long inMillis = ticket.getInTime().getTime();
+		long outMillis = ticket.getOutTime().getTime();
 
-        float duration = outMillis - inMillis;
+		float duration = outMillis - inMillis;
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration / 60 / 60 / 1000 * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration / 60 / 60 / 1000* Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
-    }
+		switch (ticket.getParkingSpot().getParkingType()) {
+			case CAR: {
+				if (duration <= 1800000) {
+					ticket.setPrice(duration / 60 / 60 / 1000 * Fare.CAR_PRICE_30MIN_OR_LESS);
+				} else {
+					ticket.setPrice(duration / 60 / 60 / 1000 * Fare.CAR_RATE_PER_HOUR);
+				}
+				break;
+			}
+			case BIKE: {
+				if (duration <= 1800000) {
+					ticket.setPrice(duration / 60 / 60 / 1000 * Fare.BIKE_PRICE_30MIN_OR_LESS);
+				} else {
+					ticket.setPrice(duration / 60 / 60 / 1000 * Fare.BIKE_RATE_PER_HOUR);
+				}
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unkown Parking Type");
+		}
+	}
 }
