184432121987178d1fb8b8b04b8c73b1a00d63a2
Jeremy Charpentier <jeremy.charpentier@posteo.net>
2022-01-31 14:07:22 +0100

    add DAO test

---
+++ src/test/java/com/parkit/parkingsystem/config/DataBaseConfigTest.java
@@ -0,0 +1,60 @@
+package com.parkit.parkingsystem.config;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class DataBaseConfigTest {
+
+    private final DataBaseConfig dataBaseConfig = new DataBaseConfig();
+
+    @Test
+    public void getConnectionTest() throws SQLException, ClassNotFoundException {
+        // GIVEN
+        Connection connection = Mockito.mock(Connection.class);
+        Mockito.mockStatic(DriverManager.class);
+        when(DriverManager.getConnection(anyString(), anyString() ,anyString())).thenReturn(connection);
+
+        // WHEN
+        Connection response = dataBaseConfig.getConnection();
+
+        // THEN
+        Assertions.assertEquals(connection, response);
+
+    }
+
+    @Test
+    public void closeConnectionTest() throws SQLException {
+        // GIVEN
+        Connection connection = Mockito.mock(Connection.class);
+
+        // WHEN
+        dataBaseConfig.closeConnection(connection);
+
+        // THEN
+        verify(connection, Mockito.times(1)).close();
+    }
+
+    @Test
+    public void closePrepareStatementTest() throws SQLException {
+        PreparedStatement preparedStatement = Mockito.mock(PreparedStatement.class);
+
+        dataBaseConfig.closePreparedStatement(preparedStatement);
+
+        verify(preparedStatement, Mockito.times(1)).close();
+
+        }
+
+}
--- src/test/java/com/parkit/parkingsystem/dao/ParkingSpotDAOTest.java
+++ src/test/java/com/parkit/parkingsystem/dao/ParkingSpotDAOTest.java
@@ -1,4 +1,39 @@
 package com.parkit.parkingsystem.dao;
 
+import com.parkit.parkingsystem.config.DataBaseConfig;
+import com.parkit.parkingsystem.constants.ParkingType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.sql.*;
+import static org.mockito.Mockito.when;
+
 public class ParkingSpotDAOTest {
+    @Mock
+    DataBaseConfig dataBaseConfig;
+    @Mock
+    PreparedStatement preparedStatement;
+    @Mock
+    ResultSet rs;
+    @Mock
+    ParkingType parkingType;
+
+    private ParkingSpotDAO parkingSpotDAO = new ParkingSpotDAO();
+
+    @Test
+    public void getNextAvailableSlotException() throws SQLException, ClassNotFoundException {
+        // GIVEN
+        preparedStatement.setString(1, parkingType.toString());
+        parkingSpotDAO.getNextAvailableSlot(ParkingType.BIKE);
+        dataBaseConfig.getConnection();
+
+        // WHEN
+        when(rs.getInt(-1)).thenThrow(Exception.class);
+
+        // THEN
+        //Assertions.assertThrows()
+
+    }
+
 }
